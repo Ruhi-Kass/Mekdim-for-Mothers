262a3b50f982c2a7cfd6599219874240aeb3f339
Tsedexashu08 <ashutsedex@gmail.com>
2026-03-31 22:23:59 +0300

    Setting up the supabase client . . .but somehow crashes for windows apps😒

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(url: " SUPABASE_URL", anonKey: "SUPABASE_ANON_KEY");
   runApp(const MainApp());
 }
-
+// for extracting the Supabase client in a variable for later uses
+final supabase = Supabase.instance.client;
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
